b6314e417674e1b60ea95c673bc77c4e16644897
ctclark <ctclark@alaska.edu>
2019-07-19 09:14:02 -0800

    Add files via upload

---
+++ R/SuessR Reference Data.R
@@ -0,0 +1,18 @@
+#' Reference data for SuessR Suess and Laws corrections
+#'
+#' @docType data
+#'
+#' @usage data(SuessR.reference.data)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Coming Soon
+#'
+#' @source Coming Soon
+#'
+#' @examples
+#' data(SuessR.reference.data)
+#'
+"SuessR.reference.data"
