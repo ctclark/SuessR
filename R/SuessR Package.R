a252c0a2502516f71a600e515201647b8b133063
Casey Clark <ctclark@alaska.edu>
2020-03-16 14:14:48 -0700

    Add files via upload

--- R/SuessR Package.R
+++ R/SuessR Package.R
@@ -3,8 +3,8 @@
 
 # Function to calculate the Laws correction. Used within the SuessR() and SuessR.custom() functions.
 
-laws.fun <- function(e1, e2, e.1, laws.CO2, P, sst, C, b) {
-  e2 + e1 - e.1 - (1/(1+((laws.CO2*P)/ (0.5*C*(1+b))))) * ((e2 - e.1)/ (b+1))
+laws.fun <- function(e1, e2, e.1, laws.CO2, sst, r, b) {
+  e2 + e1 - e.1 - (1/(1 + (laws.CO2*((1*10^-5)*(4*pi*(r*10^-6)^2)))/((0.89*1.47^((sst+273.15-303.15)/10))*(((0.216*((4/3)*pi*(r^3))^0.939)*10^-12))*(1+b))))*((e2-e.1)/(b+1))
 }
 
 
@@ -112,17 +112,17 @@ SuessR <- function(data, correct.to = 1850) {
   ref$CO2aq <- exp(ref$lnK0)*ref$fCO2
 
   # Laws expression for a given year
-  ref$laws.current <- with(ref, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, P=P, sst=sst, C=C, b=beta))
+  ref$laws.current <- with(ref, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, r=r, sst=sst, b=beta))
 
   data$order <- seq(1,length(data$id),1)
   data <- merge(data, ref, c("region", "year"), sort = F)
 
   dat1850 <- ref[ref$year==1850,]
-  dat1850$laws1850 <- with(dat1850, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, P=P, sst=sst, C=C, b=beta))
+  dat1850$laws1850 <- with(dat1850, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, r=r, sst=sst, b=beta))
 
   data <- merge(data, dat1850[,c("region", "laws1850")], "region", sort = F)
   dat.correct.to <- ref[ref$year==correct.to,]
-  dat.correct.to$laws.correct.to <- with(dat.correct.to, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, P=P, sst=sst, C=C, b=beta))
+  dat.correct.to$laws.correct.to <- with(dat.correct.to, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, r=r, sst=sst, b=beta))
   data <- merge(data, dat.correct.to[,c("region", "laws.correct.to")], "region", sort = F)
   data$Laws.cor <- round(with(data, (laws.current - laws1850) - (laws.correct.to - laws1850)),2)
 
@@ -182,9 +182,6 @@ SuessR <- function(data, correct.to = 1850) {
 #' example.region.data <- data.frame(year = seq(from = 1850, to = 2019, by = 1),
 #'                                                          region = rep("Example Region", 170),
 #'                                                          r = rep(5, 170),
-#'                                                          C = rep(1.09e-09, 170),
-#'                                                          P = rep(4.57e-10, 170),
-#'                                                          mu = rep(0.5, 170),
 #'                                                          beta = rep(0.2, 170),
 #'                                                          sst = seq(5.9, 6.6, 170),
 #'                                                          sss = seq(32.3, 32.7, 170),
@@ -253,16 +250,16 @@ SuessR.custom <- function(data, custom.region.data, correct.to = 1850) {
   ref$CO2aq <- exp(ref$lnK0)*ref$fCO2
 
   # Laws expression for a given year
-  ref$laws.current <- with(ref, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, P=P, sst=sst, C=C, b=beta))
+  ref$laws.current <- with(ref, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, r=r, sst=sst, b=beta))
 
   data$order <- seq(1,length(data$id),1)
   data <- merge(data, ref, c("region", "year"), sort = F)
 
   dat1850 <- ref[ref$year==1850,]
-  dat1850$laws1850 <- with(dat1850, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, P=P, sst=sst, C=C, b=beta))
+  dat1850$laws1850 <- with(dat1850, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, r=r, sst=sst, b=beta))
   data <- merge(data, dat1850[,c("region", "laws1850")], "region", sort = F)
   dat.correct.to <- ref[ref$year==correct.to,]
-  dat.correct.to$laws.correct.to <- with(dat.correct.to, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, P=P, sst=sst, C=C, b=beta))
+  dat.correct.to$laws.correct.to <- with(dat.correct.to, laws.fun(e1=1, e2=26.5, e.1=1,laws.CO2=CO2aq, r=r, sst=sst, b=beta))
   data <- merge(data, dat.correct.to[,c("region", "laws.correct.to")], "region", sort = F)
   data$Laws.cor <- round(with(data, (laws.current - laws1850) - (laws.correct.to - laws1850)), 2)
 
@@ -293,7 +290,7 @@ SuessR.custom <- function(data, custom.region.data, correct.to = 1850) {
 #' ideally separated by at least a decade. The function uses the magnitude (i.e., absolute value) of the observed change in
 #' DIC δ13C ('d13c.change'), the year of the first observation ('year1'), and the year of the second observation ('year2')
 #' to calculate the regional uptake constant. This value can then be supplied as part of the 'custom.region.data' argument
-#' (filling a column titled 'reg.up') for the SuessR.custom() function.
+#' (filling a column titled 'up.con') for the SuessR.custom() function.
 #' @return Returns a numerical value representing the regional uptake constant. This value can be supplied as part of the
 #' 'custom.region.data' (filling a column titled 'reg.up') arguement for the SuessR.custom() function.
 #' @examples
